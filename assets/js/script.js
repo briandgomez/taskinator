b00a95912a7f1c1e0c5daef3891d2e224c43ef00
briandgomez <briandgomez98@gmail.com>
2021-09-05 21:27:35 -0400

    dynamic created buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
+var taskIdCounter = 0;
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function(event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name'").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
     //check if input values are emtpy strings
     if (!taskNameInput || !taskTypeInput) {
         alert("You need to fill out the task form!");
@@ -24,21 +25,74 @@ var taskFormHandler = function(event) {
     createTaskE1(taskDataObj);
 };
 
-var createTaskE1 = function(taskDataObj) {
-  //create list item
-  var listItemE1 = document.createElement("li");
-  listItemE1.className = "task-item";
-  
-  //create div to hold task info and add to list item
-  var taskInfoE1 = document.createElement("div");
-  //give it a class name
-  taskInfoE1.className = "task-info";
-  //add HTML content to div
-  taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-  listItemE1.appendChild(taskInfoE1);
+var createTaskE1 = function (taskDataObj) {
+    //create list item
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
 
-  //add entire list item to list
-  tasksToDoE1.appendChild(listItemE1);  
+    //add task id as a custom attribute
+    listItemE1.setAttribute("data-task-id", taskIdCounter);
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give it a class name
+    taskInfoE1.className = "task-info";
+    //add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+
+    var taskActionsE1 = createTaskActions(taskIdCounter);
+    listItemE1.appendChild(taskActionsE1);
+
+    tasksToDoE1.appendChild(listItemE1);
+
+    //add entire list item to list
+    tasksToDoE1.appendChild(listItemE1);
+
+    //increase task counter for next unique id
+    taskIdCounter++;
+};
+
+var createTaskActions = function (taskId) {
+    //new div element with class name of "task-actions"
+    var actionContainerE1 = document.createElement("div");
+    actionContainerE1.className = "task-actions";
+
+    //created edit button
+    var editButtonE1 = document.createElement("button");
+    editButtonE1.textContent = "Edit";
+    editButtonE1.className = "btn edit-btn";
+    editButtonE1.setAttribute("data-task-id", taskId);
+
+    actionContainerE1.appendChild(editButtonE1);
+
+    //create delete button
+    var deleteButtonE1 = document.createElement("button");
+    deleteButtonE1.textContent = "Delete";
+    deleteButtonE1.className = "btn delete-btn";
+    deleteButtonE1.setAttribute = ("data-task-id", taskId);
+
+    actionContainerE1.appendChild(deleteButtonE1);
+
+    var statusSelectE1 = document.createElement("select");
+    statusSelectE1.className = "select-status";
+    statusSelectE1.setAttribute("name", "status-change");
+    statusSelectE1.setAttribute("data-task-id", taskId);
+
+    actionContainerE1.appendChild(statusSelectE1);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for (var i = 0; i < statusChoices.length; i++) {
+        //create option element
+        var statusOptionE1 = document.createElement("option");
+        statusOptionE1.textContent = statusChoices[i];
+        statusOptionE1.setAttribute("value", statusChoices[i]);
+
+        //append to select
+        statusSelectE1.appendChild(statusOptionE1);
+    }
+
+    return actionContainerE1;
 };
 
 formE1.addEventListener("submit", taskFormHandler);
